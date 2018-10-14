210187fe36ba108471f006e286c151c0c9039759
mk7293 <mxk5579@rit.edu>
2018-10-14 19:16:40 -0400

    Established basic communication between client and server

--- src/LookupServer.java
+++ src/LookupServer.java
@@ -1,7 +1,24 @@
+
+//The JSON-RPC 2.0 Base classes that define the 
+//JSON-RPC 2.0 protocol messages
+import com.thetransactioncompany.jsonrpc2.*;
+//The JSON-RPC 2.0 server framework package
+import com.thetransactioncompany.jsonrpc2.server.*;
+
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import net.minidev.json.JSONObject;
 
 /**
  * 
@@ -13,10 +30,20 @@ import java.util.HashMap;
  *
  */
 public class LookupServer {
+	private final int PORT = 8000;
 
 	// Connect with the socket
-	public LookupServer(int port) {
+	public LookupServer() {
+		try {
+			ServerSocket serverSocket = new ServerSocket(PORT);
+			System.out.println("Server Socket is listening");
+			while (true) {
+				new Handler(serverSocket.accept()).start();
+			}
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -28,7 +55,11 @@ public class LookupServer {
 	 */
 	private static class Handler extends Thread {
 
-		HashMap<Integer, InetAddress> activeNodes;
+		private static HashMap<Integer, InetAddress> activeNodes;
+		private Socket clientSocket;
+		private PrintWriter outputStream;
+		private BufferedReader inputStream;
+		private Dispatcher dispatcher;
 
 		/**
 		 * Initiate all the socket related variables
@@ -36,7 +67,23 @@ public class LookupServer {
 		 * @param socket
 		 */
 		public Handler(Socket socket) {
+			System.out.println(
+					"New client connected on PORT #: " + socket.getLocalPort() + " :: " + socket.getInetAddress());
 
+			try {
+				this.clientSocket = socket;
+				this.outputStream = new PrintWriter(clientSocket.getOutputStream());
+			} catch (IOException e) {
+				System.out.println("IO Output Exception in Handler Constructor");
+			}
+			try {
+				this.inputStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			this.dispatcher = new Dispatcher();
+			dispatcher.register(new ServerHandler.SocketHandler());
 		}
 
 		/**
@@ -45,12 +92,75 @@ public class LookupServer {
 		 */
 		public void run() {
 
+			InetAddress ipAddress = clientSocket.getInetAddress();
+			String contentHeader = "Content-Length: ";
+			int contentLength = 0;
+			try {
+				String post = inputStream.readLine();
+				boolean isPost = post.startsWith("POST");
+				System.out.println("isPost::" + isPost);
+				while (!(post = inputStream.readLine()).equals("")) {
+					if (isPost && post.startsWith(contentHeader)) {
+						contentLength = Integer.parseInt(post.substring(contentHeader.length()));
+					}
+				}
+
+				StringBuilder reqBuilder = new StringBuilder();
+
+				if (isPost) {
+					int c = 0;
+					for (int i = 0; i < contentLength; i++) {
+						c = (char) inputStream.read();
+						reqBuilder.append((char) c);
+					}
+				}
+
+				System.out.println(reqBuilder.toString());
+
+				JSONRPC2Request jsonrpc2Request = JSONRPC2Request.parse(reqBuilder.toString());
+				JSONRPC2Response jsonrpc2Response = dispatcher.process(jsonrpc2Request, null);
+				
+				System.out.println("jsonResponse.indicatesSuccess():: " + jsonrpc2Response.indicatesSuccess());
+				System.out.println("jsonResponse.indicatesSuccess():: " + jsonrpc2Response.getResult());
+				outputStream.write("HTTP/1.1 200 OK\r\n");
+				outputStream.write("Content-Type: application/json\r\n");
+				outputStream.write("\r\n");
+				outputStream.write(jsonrpc2Response.toJSONString());
+				outputStream.flush();
+				outputStream.close();
+			} catch (IOException | JSONRPC2ParseException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
 
 	public static void main(String[] args) {
-		new LookupServer(5000);
+		new LookupServer();
+	}
+
+}
+
+class ServerHandler {
+
+	public static class SocketHandler implements RequestHandler {
+
+		@Override
+		public String[] handledRequests() {
+			return new String[] { "socket" };
+		}
+
+		@Override
+		public JSONRPC2Response process(JSONRPC2Request request, MessageContext context) {
+			if (request.getMethod().equalsIgnoreCase("socket")) {
+				ArrayList<Object> params = (ArrayList) request.getParams();
+				return new JSONRPC2Response(params.get(0), request.getID());
+			} else {
+				return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, request.getID());
+			}
+		}
+
 	}
 
 }
--- src/Peers.java
+++ src/Peers.java
@@ -1,3 +1,12 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.thetransactioncompany.jsonrpc2.*;
+import com.thetransactioncompany.jsonrpc2.client.*;
 
 /**
  * 
@@ -8,22 +17,101 @@
  */
 public class Peers implements Runnable {
 
-	public Peers() {
+	private int guid;
+	private String serverIPAddress;
+	private int serverPort;
+//	private Socket socket;
+	private ObjectOutputStream outputStream;
+	private ObjectInputStream inputStream;
+	private JSONRPC2Session session;
+	private JSONRPC2Request jsonRequest;
+	private JSONRPC2Response jsonResponse;
+	private boolean isNodeOnline = false;
+
+	public Peers(int guid, String ipAddress, int port) {
+
+		this.guid = guid;
+		this.serverIPAddress = ipAddress;
+		this.serverPort = port;
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+
+			try {
+
+				System.out.println("Guid: " + guid);
+
+				System.out.println(
+						"Chord DHT Menu \n 1. Join Network \n " + "2. Leave Network \n 3. Add File \n 4. Show Files \n "
+								+ "5. Search File \n 6. Show Finger Table \n ");
+
+				int option = scanner.nextInt();
+
+				switch (option) {
+				case 1:
+					joinNetwork();
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					System.exit(0);
+					break;
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Please provide the correct option number");
+			}
+		}
 
 	}
 
-	/**
-	 * Update the finger table based on the peers activity
-	 */
-	private void constructFingerTable() {
+	private void connectToSocket() {
+		URL serverURL = null;
+		try {
+			serverURL = new URL("http://" + serverIPAddress + ":" + serverPort + "/");
+		} catch (MalformedURLException e) {
+			System.err.println("MalformedURLException occured");
+		}
+		System.out.println("To: " +serverURL.toString());
+		session = new JSONRPC2Session(serverURL);
+		System.out.println("Connect");
+	}
 
+	private void disconnectFromSocket() {
+		
 	}
 
 	/**
 	 * Join into the network
 	 */
 	private void joinNetwork() {
-
+		System.out.println("INside method");
+		if (!isNodeOnline) {
+			System.out.println("Insider Node");
+			connectToSocket();
+			ArrayList<Object> list = new ArrayList<>();
+			list.add(guid);
+			jsonRequest = new JSONRPC2Request("socket", list, 0);
+			jsonResponse = null;
+			try {
+				jsonResponse = session.send(jsonRequest);
+			} catch (JSONRPC2SessionException e) {
+				System.err.println(e.getMessage());
+			}
+			
+			System.out.println("jsonResponse.indicatesSuccess():: " + jsonResponse.indicatesSuccess());
+			if (jsonResponse.indicatesSuccess()) {
+				System.out.println(jsonResponse.getResult());
+			} else {
+				System.out.println("Error");
+			}
+		}
+		System.out.println("Exit Method");
 	}
 
 	/**
@@ -62,6 +150,13 @@ public class Peers implements Runnable {
 
 	}
 
+	/**
+	 * Update the finger table based on the peers activity
+	 */
+	private void constructFingerTable() {
+
+	}
+
 	public void run() {
 
 	}
@@ -73,8 +168,14 @@ public class Peers implements Runnable {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Peers peers = new Peers();
 
+		if (args.length != 2) {
+			System.err.println("Usage: GUID, ServerIPAddress");
+		}
+
+		new Peers(Integer.parseInt(args[0]), args[1], 8000);
 	}
 
 }
+
+

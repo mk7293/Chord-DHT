88877adce6d5267143d8b08984fdbb1b61686aec
mk7293 <mxk5579@rit.edu>
2018-10-27 13:46:03 -0400

    IMplemented anchor node and all the peers now will contact the anchor node via server

--- src/LookupServer.java
+++ src/LookupServer.java
@@ -2,30 +2,17 @@
 //The JSON-RPC 2.0 Base classes that define the 
 //JSON-RPC 2.0 protocol messages
 import com.thetransactioncompany.jsonrpc2.*;
-import com.thetransactioncompany.jsonrpc2.client.JSONRPC2Session;
-import com.thetransactioncompany.jsonrpc2.client.JSONRPC2SessionException;
 //The JSON-RPC 2.0 server framework package
-import com.thetransactioncompany.jsonrpc2.server.*;
 
 import java.io.BufferedReader;
-import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
-import net.minidev.json.JSONObject;
 
 /**
  * 
@@ -40,6 +27,7 @@ public class LookupServer {
 	private final int PORT = 8000;
 
 	// Connect with the socket
+	@SuppressWarnings("resource")
 	public LookupServer() {
 		try {
 			ServerSocket serverSocket = new ServerSocket(PORT);
@@ -65,9 +53,8 @@ public class LookupServer {
 		private Socket clientSocket;
 		private PrintWriter outputStream;
 		private BufferedReader inputStream;
-		private static TreeMap<Integer, InetAddress> activeNodes = new TreeMap<>();
 		private static HashMap<Integer, InetAddress> anchorNodes = new HashMap<>();
-		private static int requestId = 0;
+		private static InetAddress anchorIpAddress;
 
 		/**
 		 * Initiate all the socket related variables
@@ -93,23 +80,13 @@ public class LookupServer {
 		 */
 		public void run() {
 
-			if (anchorNodes.isEmpty()) {
-			
-			}
-			
-			
-			
-			
-			
-			
-			
 			String contentHeader = "Content-Length: ";
 			int contentLength = 0;
 
 			try {
 				String post = inputStream.readLine();
 				boolean isPost = post.startsWith("POST");
-				System.out.println("isPost::" + isPost);
+
 				while (!(post = inputStream.readLine()).equals("")) {
 					if (isPost && post.startsWith(contentHeader)) {
 						contentLength = Integer.parseInt(post.substring(contentHeader.length()));
@@ -127,7 +104,7 @@ public class LookupServer {
 				}
 
 				JSONRPC2Request jsonrpc2Request = JSONRPC2Request.parse(reqBuilder.toString());
-				JSONRPC2Response jsonrpc2Response = processMethods(jsonrpc2Request, clientSocket.getInetAddress());
+				JSONRPC2Response jsonrpc2Response = processMethods(jsonrpc2Request);
 
 				outputStream.write("HTTP/1.1 200 OK\r\n");
 				outputStream.write("Content-Type: application/json\r\n");
@@ -138,67 +115,51 @@ public class LookupServer {
 				outputStream.close();
 				clientSocket.close();
 
-				sendActiveNodes();
-
 			} catch (IOException | JSONRPC2ParseException e) {
 				e.printStackTrace();
 			}
 		}
 
 		@SuppressWarnings({ "unchecked", "deprecation" })
-		public JSONRPC2Response processMethods(JSONRPC2Request request, InetAddress ipAddr) {
+		public JSONRPC2Response processMethods(JSONRPC2Request request) {
 
 			String response = "";
+			ArrayList<Object> params;
+			
 			switch (request.getMethod()) {
 			case "join":
-				ArrayList<Object> params = (ArrayList<Object>) request.getParams();
+				params = (ArrayList<Object>) request.getParams();
 
 				long lo = (long) params.get(0);
 				int guid = (int) lo;
 
-				if (!activeNodes.containsKey(guid)) {
-					activeNodes.put(guid, ipAddr);
-					response = "Welcome GUID: " + guid;
+				if (anchorNodes.isEmpty()) {
+					synchronized (anchorNodes) {
+						anchorIpAddress = clientSocket.getInetAddress();
+						anchorNodes.put(guid, anchorIpAddress);
+						response = "Welcome Anchor GUID: " + guid;
+					}
 				} else {
-					response = "GUID: " + guid + " already in use";
+					response = String.valueOf(anchorIpAddress);
 				}
+				
+				break;
 
 			case "leave":
+				
+				params = (ArrayList<Object>) request.getPositionalParams();
+				guid = Integer.parseInt((String) params.get(0));
+				
+				if (anchorNodes.containsKey(guid)) {
+					
+				} else {
+					response = String.valueOf(anchorIpAddress);
+				}
+				
 				break;
 			}
 
 			return new JSONRPC2Response(response, request.getID());
-		}
-
-		public void sendActiveNodes() {
-			Map<String, Object> temps = new TreeMap<String, Object>();
-			for (Map.Entry<Integer, InetAddress> entry : activeNodes.entrySet()) {
-				temps.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
-			}
-			
-			for (Map.Entry<Integer, InetAddress> entry : activeNodes.entrySet()) {
-				System.out.println("ipaddr:: " + entry.getValue());
-				try {
-					JSONRPC2Session session = new JSONRPC2Session(new URL("http:/" + entry.getValue() + ":4000/"));
-					System.out.println("size:: " + temps.size() + "::::" + temps.get("1"));
-					
-					JSONRPC2Request request = new JSONRPC2Request("UpdateFingerTable", requestId++);
-					request.setNamedParams(temps);
-					JSONRPC2Response jsonrpc2Response = session.send(request);
-
-					if (jsonrpc2Response.indicatesSuccess()) {
-						System.out.println("ActiveNodes send to client: " + entry.getValue() + " and "
-								+ jsonrpc2Response.getResult());
-					} else {
-						System.out.println("Error");
-					}
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (JSONRPC2SessionException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
--- src/Peers.java
+++ src/Peers.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.*;
 import java.util.ArrayList;
@@ -13,8 +11,6 @@ import java.util.TreeMap;
 
 import com.thetransactioncompany.jsonrpc2.*;
 import com.thetransactioncompany.jsonrpc2.client.*;
-import com.thetransactioncompany.jsonrpc2.server.Dispatcher;
-import com.thetransactioncompany.jsonrpc2.util.NamedParamsRetriever;
 
 /**
  * 
@@ -40,10 +36,13 @@ public class Peers extends Thread {
 	private boolean isNodeOnline = false;
 
 	ArrayList<String> fileCollection;
+	private static TreeMap<Integer, InetAddress> anchorActiveNodes = new TreeMap<>();
 	TreeMap<Integer, InetAddress> activeNodes;
 
 	InetAddress routeAddr = null;
 	String routeMessage = "";
+
+	Scanner scanner = new Scanner(System.in);
 
 	public Peers(int guid, String ipAddress, int port) {
 
@@ -58,7 +57,6 @@ public class Peers extends Thread {
 		activeNodes = new TreeMap<>();
 
 		while (true) {
-			Scanner scanner = new Scanner(System.in);
 
 			try {
 
@@ -66,38 +64,55 @@ public class Peers extends Thread {
 						"Chord DHT Menu \n 1. Join Network \n " + "2. Leave Network \n 3. Add File \n 4. Show Files \n "
 								+ "5. Search File \n 6. Show Finger Table \n ");
 
+				System.out.print("Please enter your option number: ");
 				int option = scanner.nextInt();
+				System.out.println();
 
 				switch (option) {
 				case 1:
 					joinNetwork();
-					if (!isNodeOnline) {
-						System.out.println("Please provide different GUID between 0 & 15 inclusive: ");
-						this.guid = scanner.nextInt();
-					}
 					break;
 				case 2:
-
+					if (isNodeOnline) {
+						leaveNetwork();
+					} else {
+						System.out.println("Node is offline");
+					}
 					break;
 				case 3:
-					System.out.println("Please enter the file content: ");
-					String fileContent = scanner.next();
+					if (isNodeOnline) {
+						System.out.println("Please enter the file content: ");
+						String fileContent = scanner.next();
 
-					addFile(-1, fileContent);
-
+						addFile(-1, fileContent);
+					} else {
+						System.out.println("Node is offline");
+					}
 					break;
 				case 4:
-					showFilesFromCurrent();
+					if (isNodeOnline) {
+						showFilesFromCurrent();
+					} else {
+						System.out.println("Node is offline");
+					}
 
 					break;
 				case 5:
-					System.out.println("Please enter the file content: ");
-					String searchFileContent = scanner.next();
+					if (isNodeOnline) {
+						System.out.println("Please enter the file content: ");
+						String searchFileContent = scanner.next();
 
-					searchFile(searchFileContent);
+						searchFile(searchFileContent);
+					} else {
+						System.out.println("Node is offline");
+					}
 					break;
 				case 6:
-					fingerTable.printFingerTable();
+					if (isNodeOnline) {
+						fingerTable.printFingerTable();
+					} else {
+						System.out.println("Node is offline");
+					}
 					break;
 				}
 
@@ -105,7 +120,6 @@ public class Peers extends Thread {
 				System.out.println("Please provide the correct option number");
 			}
 		}
-
 	}
 
 	private void connectToSocket() {
@@ -123,38 +137,67 @@ public class Peers extends Thread {
 	 */
 	private void joinNetwork() {
 		if (!isNodeOnline) {
-
-			connectToSocket();
-			ArrayList<Object> list = new ArrayList<>();
-			list.add(guid);
-
-			jsonRequest = new JSONRPC2Request("join", list, requestId++);
-			jsonResponse = null;
 			try {
+				connectToSocket();
+				ArrayList<Object> list = new ArrayList<>();
+				list.add(guid);
+
+				jsonRequest = new JSONRPC2Request("join", list, requestId++);
 				jsonResponse = session.send(jsonRequest);
+
+				String response = "";
+
+				if (jsonResponse.indicatesSuccess()) {
+					response = (String) jsonResponse.getResult();
+					System.out.println("JSONResponse from server: " + response);
+				} else {
+					System.out.println("Error");
+				}
+
+				if (response.equalsIgnoreCase("Welcome Anchor GUID: " + guid)) {
+					System.out.println("Anchor Node GUID: " + guid + " joined");
+					anchorActiveNodes.put(guid, InetAddress.getByName(InetAddress.getLocalHost().getHostAddress()));
+					isNodeOnline = true;
+
+					constructFingerTable(anchorActiveNodes);
+					getSuccessorFiles();
+
+					System.out.println();
+				} else {
+					System.out.println("Response:: " + response);
+					JSONRPC2Session rpcSession = new JSONRPC2Session(new URL("http:/" + response + ":4000/"));
+					JSONRPC2Request request2 = new JSONRPC2Request("JoinPeers", requestId++);
+
+					list.clear();
+					list.add(String.valueOf(guid));
+					list.add(String.valueOf(InetAddress.getLocalHost().getHostAddress()));
+					request2.setPositionalParams(list);
+
+					JSONRPC2Response response2 = rpcSession.send(request2);
+
+					if (response2.indicatesSuccess()) {
+						response = (String) response2.getResult();
+						if (response.equalsIgnoreCase("Welcome GUID: " + guid)) {
+							System.out.println("GUID: " + guid + " joined along with peers");
+							isNodeOnline = true;
+							System.out.println();
+						} else {
+							System.out.println(response);
+							isNodeOnline = false;
+							System.out.println("Please provide different GUID between 0 & 15 inclusive: ");
+							this.guid = scanner.nextInt();
+						}
+					} else {
+						System.out.println("Error");
+					}
+				}
 			} catch (JSONRPC2SessionException e) {
-				System.err.println(e.getMessage());
+				System.err.println("JSONRPC2SessionException on JoinNetwork Method");
+			} catch (UnknownHostException e) {
+				System.err.println("UnknownHostException on JoinNetwork Method");
+			} catch (MalformedURLException e2) {
+				System.err.println("MalformedURLException on JoinNetwork Method");
 			}
-
-			String response = "";
-
-			if (jsonResponse.indicatesSuccess()) {
-				response = (String) jsonResponse.getResult();
-				System.out.println("JSONResponse from server: " + response);
-			} else {
-				System.out.println("Error");
-			}
-
-			if (response.equalsIgnoreCase("Welcome GUID: " + guid)) {
-				System.out.println("GUID: " + guid + " joined along with peers");
-				isNodeOnline = true;
-				System.out.println();
-
-			} else {
-				System.out.println("GUID already exists");
-				isNodeOnline = false;
-			}
-
 		} else {
 			System.out.println("Network '" + guid + "' already joined ");
 		}
@@ -165,6 +208,50 @@ public class Peers extends Thread {
 	 */
 	private void leaveNetwork() {
 
+		connectToSocket();
+		try {
+			ArrayList<Object> list = new ArrayList<>();
+			list.add(String.valueOf(guid));
+
+			jsonRequest = new JSONRPC2Request("leave", requestId++);
+			jsonRequest.setPositionalParams(list);
+			jsonResponse = session.send(jsonRequest);
+
+			String response = "";
+
+			if (jsonResponse.indicatesSuccess()) {
+				response = (String) jsonResponse.getResult();
+				System.out.println("JSONResponse from server: " + response);
+			} else {
+				System.out.println("Error");
+			}
+
+			System.out.println("succ:: " + fingerTable.getFingerTable().get(fingerTable.getFirstNode()) + ":::: "
+					+ fingerTable.getFirstNode());
+			transferSuccessorFiles(fingerTable.getFingerTable().get(fingerTable.getFirstNode()));
+
+			// If else block
+			System.out.println("Response:: " + response);
+			JSONRPC2Session rpcSession = new JSONRPC2Session(new URL("http:/" + response + ":4000/"));
+			JSONRPC2Request request2 = new JSONRPC2Request("LeavePeer", requestId++);
+
+			list.clear();
+			list.add(String.valueOf(guid));
+			request2.setPositionalParams(list);
+
+			JSONRPC2Response response2 = rpcSession.send(request2);
+
+			isNodeOnline = false;
+
+			if (response2.indicatesSuccess()) {
+				System.out.println("Node leaves network");
+			}
+
+		} catch (JSONRPC2SessionException e) {
+			System.out.println("JSONRPC2SessionException on leave network()");
+		} catch (MalformedURLException e) {
+			System.out.println("MalformedURLException on leave network()");
+		}
 	}
 
 	private int findDistance(int keyId, int destNodeId) {
@@ -214,9 +301,9 @@ public class Peers extends Thread {
 			}
 
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			System.out.println("MalformedURLException on transferFile()");
 		} catch (JSONRPC2SessionException e) {
-			e.printStackTrace();
+			System.out.println("JSONRPC2SessionException on transferFile()");
 		}
 
 	}
@@ -301,9 +388,9 @@ public class Peers extends Thread {
 			}
 
 		} catch (MalformedURLException e) {
-			e.printStackTrace();
+			System.out.println("MalformedURLException on searchSuccessorPeers()");
 		} catch (JSONRPC2SessionException e) {
-			e.printStackTrace();
+			System.out.println("JSONRPC2SessionException on searchSuccessorPeers()");
 		}
 	}
 
@@ -348,18 +435,23 @@ public class Peers extends Thread {
 	 */
 	private void searchFile(String searchFileContent) {
 
-		int hashId = Math.abs(searchFileContent.hashCode() % 16);
-		
-		System.out.println("hashId: " + hashId);
-
-		if (hashId == guid) {
-			if (fileCollection.contains(searchFileContent)) {
-				System.out.println("File: " + searchFileContent + " is at guid: " + guid);
-			} else {
-				System.out.println("No such File exists");
-			}
+		if (fileCollection.contains(searchFileContent)) {
+			System.out.println("File: " + searchFileContent + " is at guid: " + guid);
 		} else {
-			searchFileInOtherPeers(hashId, searchFileContent, activeNodes.get(guid));
+
+			int hashId = Math.abs(searchFileContent.hashCode() % 16);
+
+			System.out.println("hashId: " + hashId);
+
+			if (hashId == guid) {
+				if (fileCollection.contains(searchFileContent)) {
+					System.out.println("File: " + searchFileContent + " is at guid: " + guid);
+				} else {
+					System.out.println("No such File exists");
+				}
+			} else {
+				searchFileInOtherPeers(hashId, searchFileContent, activeNodes.get(guid));
+			}
 		}
 	}
 
@@ -375,13 +467,56 @@ public class Peers extends Thread {
 	}
 
 	/**
+	 * Transfer all the current node files to the successors when the node goes
+	 * offline
+	 */
+	private void transferSuccessorFiles(int successorNode) {
+
+		try {
+
+			System.out.println("activeNodes.get(successorNode)" + activeNodes.get(successorNode));
+
+			JSONRPC2Session rpcSession = new JSONRPC2Session(
+					new URL("http:/" + activeNodes.get(successorNode) + ":4000"));
+
+			JSONRPC2Request request = new JSONRPC2Request("TransferAllFiles", requestId++);
+			ArrayList<Object> list = new ArrayList<>();
+
+			for (String string : fileCollection) {
+				list.add(string);
+			}
+
+			fileCollection.clear();
+
+			request.setPositionalParams(list);
+
+			JSONRPC2Response response = rpcSession.send(request);
+
+			if (response.indicatesSuccess()) {
+				System.out.println(response.getResult());
+			} else {
+				System.out.println("Error");
+			}
+
+		} catch (MalformedURLException e) {
+			System.out.println("MalformedURLException on transferSuccessorFiles()");
+		} catch (JSONRPC2SessionException e) {
+			System.out.println("JSONRPC2SessionException on transferSuccessorFiles()");
+		}
+
+	}
+
+	/**
 	 * Get the file from the successor when the nodes comes online
 	 */
 	private void getSuccessorFiles() {
+		System.out.println("Inside getSuccessorFiles()");
 		if (isNodeOnline) {
 			int successorNode = fingerTable.getFingerTable().get(fingerTable.getFirstNode());
 			if (guid != successorNode) {
+				System.out.println(activeNodes.get(successorNode));
 				try {
+
 					JSONRPC2Session session = new JSONRPC2Session(
 							new URL("http:/" + activeNodes.get(successorNode) + ":4000"));
 
@@ -410,9 +545,9 @@ public class Peers extends Thread {
 					}
 
 				} catch (MalformedURLException e) {
-					e.printStackTrace();
+					System.out.println("MalformedURLException on getSuccessorFiles()");
 				} catch (JSONRPC2SessionException e) {
-					e.printStackTrace();
+					System.out.println("JSONRPC2SessionException on getSuccessorFiles()");
 				}
 			}
 		}
@@ -422,34 +557,97 @@ public class Peers extends Thread {
 	 * Update the finger table based on the peers activity
 	 */
 	private void constructFingerTable(TreeMap<Integer, InetAddress> activeNodes) {
+
 		fingerTable = new FingerTable(guid, activeNodes);
 	}
 
-	@SuppressWarnings({ "unchecked", "deprecation" })
+	public void sendActiveNodes() {
+		Map<String, Object> temps = new TreeMap<String, Object>();
+		for (Map.Entry<Integer, InetAddress> entry : anchorActiveNodes.entrySet()) {
+			activeNodes.put(entry.getKey(), entry.getValue());
+			temps.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+		}
+
+		for (Map.Entry<Integer, InetAddress> entry : anchorActiveNodes.entrySet()) {
+
+			if (entry.getKey() == guid) {
+				constructFingerTable(anchorActiveNodes);
+				continue;
+			}
+
+			try {
+				JSONRPC2Session rpcSession = new JSONRPC2Session(new URL("http:/" + entry.getValue() + ":4000/"));
+
+				JSONRPC2Request request = new JSONRPC2Request("UpdateFingerTable", requestId++);
+				request.setNamedParams(temps);
+				JSONRPC2Response jsonrpc2Response = rpcSession.send(request);
+
+				if (jsonrpc2Response.indicatesSuccess()) {
+					System.out.println(
+							"ActiveNodes send to client: " + entry.getValue() + " and " + jsonrpc2Response.getResult());
+				} else {
+					System.out.println("Error");
+				}
+
+			} catch (IOException e) {
+				System.out.println("IOException on sendActiveNodes()");
+			} catch (JSONRPC2SessionException e) {
+				System.out.println("JSONRPC2SessionException on sendActiveNodes()");
+			}
+		}
+	}
+
 	private JSONRPC2Response processMethods(JSONRPC2Request request) {
 		String response = "";
 
 		ArrayList<Object> tempList;
 
 		switch (request.getMethod()) {
+		case "JoinPeers":
+			tempList = (ArrayList<Object>) request.getPositionalParams();
+
+			int peerGuid = Integer.parseInt((String) tempList.get(0));
+			String ip = (String) tempList.get(1);
+			try {
+				InetAddress address = InetAddress.getByName(ip);
+				if (!anchorActiveNodes.containsKey(peerGuid)) {
+					synchronized (anchorActiveNodes) {
+						anchorActiveNodes.put(peerGuid, address);
+					}
+					response = "Welcome GUID: " + peerGuid;
+				} else {
+					response = "GUID: " + guid + " already in use";
+				}
+			} catch (UnknownHostException e1) {
+				System.out.println("UnknownHostException on processMethods()");
+			}
+			break;
+		case "LeavePeer":
+			tempList = (ArrayList<Object>) request.getPositionalParams();
+			peerGuid = Integer.parseInt((String) tempList.get(0));
+
+			synchronized (anchorActiveNodes) {
+				anchorActiveNodes.remove(peerGuid);
+			}
+
+			break;
 		case "UpdateFingerTable":
 
 			Map<String, Object> tempMap = request.getNamedParams();
-
+			activeNodes.clear();
 			for (Map.Entry<String, Object> entry : tempMap.entrySet()) {
 
 				try {
 					activeNodes.put(Integer.parseInt(entry.getKey()),
 							InetAddress.getByName(String.valueOf(entry.getValue()).substring(1)));
 				} catch (NumberFormatException | UnknownHostException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					System.out.println("UnknownHostException on processMethods()");
 				}
 			}
 
 			constructFingerTable(activeNodes);
 			response = "Finger Table Constructed";
-			getSuccessorFiles();
+
 			break;
 		case "GetAllFiles":
 			if (isNodeOnline && !fileCollection.isEmpty()) {
@@ -474,6 +672,13 @@ public class Peers extends Thread {
 			}
 
 			break;
+		case "TransferAllFiles":
+			tempList = (ArrayList<Object>) request.getPositionalParams();
+			for (Object object : tempList) {
+				fileCollection.add((String) object);
+			}
+
+			break;
 		case "InsertTransferedFile":
 			tempList = (ArrayList<Object>) request.getPositionalParams();
 
@@ -488,10 +693,10 @@ public class Peers extends Thread {
 			int fileId = Integer.parseInt((String) tempList.get(0));
 			String searchFileContent = (String) tempList.get(1);
 			try {
-				String ip = (String) tempList.get(2);
+				ip = (String) tempList.get(2);
 				routeAddr = InetAddress.getByName(ip.substring(1));
 			} catch (UnknownHostException e) {
-				e.printStackTrace();
+				System.out.println("UnknownHostException on processMethods()");
 			}
 
 			if (fileId == guid) {
@@ -506,13 +711,13 @@ public class Peers extends Thread {
 			break;
 		case "PrintResponse":
 			tempList = (ArrayList<Object>) request.getPositionalParams();
-
 			System.out.println((String) tempList.get(0));
 		}
 
 		return new JSONRPC2Response(response, request.getID());
 	}
 
+	@SuppressWarnings("resource")
 	@Override
 	public void run() {
 		try {
@@ -520,7 +725,6 @@ public class Peers extends Thread {
 
 			while (true) {
 				Socket socket = serverSocket.accept();
-//				System.out.println("Connected run method:: " + socket.getInetAddress());
 				PrintWriter outputStream = new PrintWriter(socket.getOutputStream());
 				BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
@@ -548,6 +752,8 @@ public class Peers extends Thread {
 				JSONRPC2Request jsonrpc2Request = JSONRPC2Request.parse(reqBuilder.toString());
 				JSONRPC2Response jsonrpc2Response = processMethods(jsonrpc2Request);
 
+				String peerMethod = jsonrpc2Request.getMethod();
+
 				outputStream.write("HTTP/1.1 200 OK\r\n");
 				outputStream.write("Content-Type: application/json\r\n");
 				outputStream.write("\r\n");
@@ -557,38 +763,44 @@ public class Peers extends Thread {
 
 				socket.close();
 
+				if (peerMethod.equalsIgnoreCase("JoinPeers") || peerMethod.equalsIgnoreCase("LeavePeer")) {
+					System.out.println(" \n Sending Live Nodes");
+					sendActiveNodes();
+				}
+
+				if (peerMethod.equalsIgnoreCase("UpdateFingerTable")) {
+					System.out.println(" \n Getting Nodes from Successor");
+					getSuccessorFiles();
+				}
+
 				if (routeMessage.length() != 0) {
-					try {
-						JSONRPC2Session peerSession = new JSONRPC2Session(new URL("http:/" + routeAddr + ":4000"));
+					JSONRPC2Session peerSession = new JSONRPC2Session(new URL("http:/" + routeAddr + ":4000"));
 
-						JSONRPC2Request peerRequest = new JSONRPC2Request("PrintResponse", requestId++);
+					JSONRPC2Request peerRequest = new JSONRPC2Request("PrintResponse", requestId++);
 
-						ArrayList<Object> list = new ArrayList<>();
+					ArrayList<Object> list = new ArrayList<>();
 
-						list.add(routeMessage);
-						peerRequest.setPositionalParams(list);
+					list.add(routeMessage);
+					peerRequest.setPositionalParams(list);
 
-						JSONRPC2Response peerResponse = peerSession.send(peerRequest);
+					JSONRPC2Response peerResponse = peerSession.send(peerRequest);
 
-						if (peerResponse.indicatesSuccess()) {
-							System.out.println(peerResponse.getResult());
-						} else {
-							System.out.println("Error in jsonResponse from searchSuccessorPeers() method");
-						}
-
-						routeMessage = "";
-
-					} catch (MalformedURLException e) {
-						e.printStackTrace();
-					} catch (JSONRPC2SessionException e) {
-						e.printStackTrace();
+					if (peerResponse.indicatesSuccess()) {
+						System.out.println(peerResponse.getResult());
+					} else {
+						System.out.println("Error in jsonResponse from searchSuccessorPeers() method");
 					}
+
+					routeMessage = "";
+
 				}
 			}
 		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("IOException on run()");
 		} catch (JSONRPC2ParseException e) {
-			e.printStackTrace();
+			System.out.println("JSONRPC2ParseException on run()");
+		} catch (JSONRPC2SessionException e) {
+			System.out.println("JSONRPC2SessionException on run()");
 		}
 	}
 
@@ -604,7 +816,7 @@ public class Peers extends Thread {
 			System.err.println("Usage: GUID, ServerIPAddress");
 		}
 
-		Peers peers = new Peers(Integer.parseInt(args[0]), args[1], 8000);
+		new Peers(Integer.parseInt(args[0]), args[1], 8000);
 //		new Peers(1, "127.0.0.1", 5014);
 
 	}
